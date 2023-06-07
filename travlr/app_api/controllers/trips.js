28a8fa21dd459ef4888d312239c369e8c770a7fb
TiciaD <dunnticia633@gmail.com>
2023-06-06 23:47:21 -0400

    Module 6 - SPA: Create Angular Admin Site   - Develop Trip list component   - Refactor Trip logic into TripCard component   - Develop Trip data service to interact with the API on the Express backend   - Add appropriate CRUD methods to the API backend, POST, GET, DELETE, PUT   - Retrieve and display a list of trips

--- travlr/app_api/controllers/trips.js
+++ travlr/app_api/controllers/trips.js
@@ -3,6 +3,7 @@ const Model = mongoose.model('trips');
 
 // GET: /trips - lists all the trips
 const tripsList = async (req, res) => {
+  console.log('Trips list invoked with: ', req)
   // empty filter for all
   Model.find({}).exec((err, trips) => {
       if (!trips) {
@@ -17,6 +18,7 @@ const tripsList = async (req, res) => {
 
 // GET: /trips/:tripCode - returns a single trip
 const tripsFindCode = async (req, res) => {
+  console.log('tripsFindCode invoked with ' + req.params.tripCode)
   Model.find(
       {'code': req.params.tripCode}
     ).exec((err, trip) => {
@@ -30,7 +32,58 @@ const tripsFindCode = async (req, res) => {
   });
 };
 
+const tripsAddTrip = async (req, res) => {
+  Model.create({
+    code: req.body.code,
+    name: req.body.name,
+    length: req.body.length,
+    start: req.body.start,
+    resort: req.body.resort,
+    perPerson: req.body.perPerson,
+    image: req.body.image,
+    description: req.body.description
+  },
+  (err, trip) => {
+    if (err) {
+      return res.status(400).json(err); // bad request, invalid content
+    } else {
+      return res.status(201).json(trip); // created
+    }
+  }
+  )
+}
+
+const tripsUpdateTrip = async (req, res) => {
+  console.log(req.body);
+  Model.findOneAndUpdate({'code': req.params.tripCode}, {
+    code: req.bode.code,
+    name: req.body.name,
+    length: req.body.length,
+    start: req.body.start,
+    resort: req.body.resort,
+    perPerson: req.body.perPerson,
+    image: req.body.image,
+    description: req.body.description
+  }, {new: true}).then((trip => {
+    if (!trip) {
+      return res.status(404).send({
+        message: "Trip not found with code " + req.params.tripCode
+      })
+    }
+    res.send(trip)
+  }).catch(err => {
+    if (err.kind === 'ObjectId') {
+      return res.status(404).send({
+        message: "Trip not found with code " + req.params.tripCode
+      });
+    }
+    return res.status(500).json(err) // server error
+  }))
+}
+
 module.exports = {
   tripsList,
-  tripsFindCode
+  tripsFindCode,
+  tripsAddTrip,
+  tripsUpdateTrip
 };
