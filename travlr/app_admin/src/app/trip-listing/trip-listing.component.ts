d44cbacd69b52a45f1c0af49f697eb8f390b661b
TiciaD <dunnticia633@gmail.com>
2023-06-13 15:59:30 -0400

    Module 7 - Add Authentication to Express Backend and Angular SPA frontend   - added layer of security to the SPA admin side including     - HTML login form for the admin site     - logic to secure the administrative endpoint methods   - Added packages for jsonwebtoken, crypto, dotenv, passport, passport-local and express-jwt   - Create a navigation bar component to hold the "Log in” link   - Create a new homepage with a message prompting the user to log in   - Dynamically show Add Trip or Edit Trip based on user authentication state

--- travlr/app_admin/src/app/trip-listing/trip-listing.component.ts
+++ travlr/app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
-import { TripDataService } from "services/trip-data.service";
-import { Trip } from "models/trip";
+import { TripDataService } from "src/app/services/trip-data.service";
+import { Trip } from "src/app/models/trip";
+import { AuthenticationService } from "../services/authentication.service";
 
 @Component({
   selector: "app-trip-listing",
@@ -15,7 +16,8 @@ export class TripListingComponent implements OnInit {
 
   constructor(
     private tripDataService: TripDataService,
-    private router: Router
+    private router: Router,
+    private authenticationService: AuthenticationService
   ) {}
 
   private addTrip(): void {
@@ -33,5 +35,8 @@ export class TripListingComponent implements OnInit {
 
   ngOnInit(): void {
     this.getTrips();
+  }
+  public isLoggedIn(): boolean {
+    return this.authenticationService.isLoggedIn();
   }
 }
